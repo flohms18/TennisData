888400e6c9c51a5f0781a11e898faab9bbf7f720
flotr18 <trouilletflorent@outlook.com>
2025-02-25 20:19:21 +0100

    Nadal v Djokovic

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ year = input("Select year to analyse please: YYYY ")
 
 df = pd.read_csv('AptSeason/atp_matches_' + year + '.csv')
 
-rg = df[df['tourney_name'] == 'Roland Garros']
+Nadal = df[(df['winner_name'] == 'Rafael Nadal') & (df['loser_name'] == 'Novak Djokovic')]
+Djokovic = df[(df['winner_name'] == 'Novak Djokovic') & (df['loser_name'] == 'Rafael Nadal')]
 
-
-
-print(rg)
+print("Nadal won ! " + Djokovic['score'])
+print("Djokovic won ! " + Djokovic['score'])
