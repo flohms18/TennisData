fc5029edfda967db99d35c26b022ea06dd6e0c1a
flotr18 <trouilletflorent@outlook.com>
2025-02-25 21:24:19 +0100

    H2d

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 year = input("Select year to analyse please: YYYY ")
 
+
+
 df = pd.read_csv('AptSeason/atp_matches_' + year + '.csv')
 
 Nadal = df[(df['winner_name'] == 'Rafael Nadal') & (df['loser_name'] == 'Novak Djokovic')]
 Djokovic = df[(df['winner_name'] == 'Novak Djokovic') & (df['loser_name'] == 'Rafael Nadal')]
 
-print("Nadal won ! " + Djokovic['score'])
-print("Djokovic won ! " + Djokovic['score'])
+CountP1 = len(Nadal)
+CountP2 = len(Djokovic)
+
+print(f"For {year} Nadal won {CountP1} times, Djoko won {CountP2} times !")
