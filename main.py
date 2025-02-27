429018fa96c547687d523a78afad658e67d3dc6e
flotr18 <trouilletflorent@outlook.com>
2025-02-27 14:18:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ for x in range(2000,2025):
     df = pd.read_csv('AptSeason/atp_matches_' + str(x) + '.csv')
     Nadal = df[(df['winner_name'] == P1Name) & (df['loser_name'] == P2Name)]
     Djokovic = df[(df['winner_name'] == P2Name) & (df['loser_name'] == P1Name)]
-    CountP1 = len(Nadal)
-    CountP2 = len(Djokovic)
-print(f"In {x} {P1Name} won {CountP1} times, {P2Name} won {CountP2} times !")
+    CountP1 =+ len(Nadal)
+    CountP2 =+ len(Djokovic)
+    print(f"In {x} {P1Name} won {CountP1} times, {P2Name} won {CountP2} times !")
+print(CountP1)
+print(CountP2)
