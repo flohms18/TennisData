f2d36e01b2a8416ee5dbcf172e0fdabcb61cff58
flotr18 <trouilletflorent@outlook.com>
2025-02-27 18:30:18 +0100

    Add If conctions

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ for x in range(2000,2025):
     df = pd.read_csv('AptSeason/atp_matches_' + str(x) + '.csv')
     Nadal = df[(df['winner_name'] == P1Name) & (df['loser_name'] == P2Name)]
     Djokovic = df[(df['winner_name'] == P2Name) & (df['loser_name'] == P1Name)]
-    CountP1 =+ len(Nadal)
-    CountP2 =+ len(Djokovic)
-    print(f"In {x} {P1Name} won {CountP1} times, {P2Name} won {CountP2} times !")
-    TotalP1 += len(Nadal)
-    TotalP2 += len(Djokovic)
+    if Nadal.any().any() == True or Djokovic.any().any() == True :
+
+        CountP1 =+ len(Nadal)
+        CountP2 =+ len(Djokovic)
+        print(f"In {x} {P1Name} won {CountP1} times, {P2Name} won {CountP2} times !")
+        TotalP1 += len(Nadal)
+        TotalP2 += len(Djokovic)
 print(f"{P1Name} won {TotalP1} times, while {P2Name} won {TotalP2} times !")
 
 
