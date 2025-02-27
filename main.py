f22fa7eae83ee9749ffbbda1892396eb3ea79b8c
flotr18 <trouilletflorent@outlook.com>
2025-02-27 13:51:52 +0100

    Add files

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import pandas as pd
 
-year = input("Select year to analyse please: YYYY ")
 P1Name = input("Select P1")
 P2Name = input("Select P2")
 
-
-
-
-df = pd.read_csv('AptSeason/atp_matches_' + year + '.csv')
-
-Nadal = df[(df['winner_name'] == P1Name) & (df['loser_name'] == P2Name)]
-Djokovic = df[(df['winner_name'] == P2Name) & (df['loser_name'] == P1Name)]
-
-CountP1 = len(Nadal)
-CountP2 = len(Djokovic)
-
-print(f"For {year} {P1Name} won {CountP1} times, {P2Name} won {CountP2} times !")
+for x in range(2000,2025):
+    df = pd.read_csv('AptSeason/atp_matches_' + str(x) + '.csv')
+    Nadal = df[(df['winner_name'] == P1Name) & (df['loser_name'] == P2Name)]
+    Djokovic = df[(df['winner_name'] == P2Name) & (df['loser_name'] == P1Name)]
+    CountP1 = len(Nadal)
+    CountP2 = len(Djokovic)
+print(f"In {x} {P1Name} won {CountP1} times, {P2Name} won {CountP2} times !")
