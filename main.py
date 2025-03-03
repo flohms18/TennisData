21569b9f7d6ff09f926fdcb2acbb7978a08f91ee
flotr18 <trouilletflorent@outlook.com>
2025-03-03 10:57:15 +0100

    Changed Var

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ TotalP2 = 0
 
 for x in range(2000,2025):
     df = pd.read_csv('AptSeason/atp_matches_' + str(x) + '.csv')
-    Nadal = df[(df['winner_name'] == P1Name) & (df['loser_name'] == P2Name)]
-    Djokovic = df[(df['winner_name'] == P2Name) & (df['loser_name'] == P1Name)]
-    if Nadal.values.any() == True or Djokovic.values.any() == True :
-        CountP1 =+ len(Nadal)
-        CountP2 =+ len(Djokovic)
+    Player1 = df[(df['winner_name'] == P1Name) & (df['loser_name'] == P2Name)]
+    Player2 = df[(df['winner_name'] == P2Name) & (df['loser_name'] == P1Name)]
+    if Player1.values.any() == True or Player2.values.any() == True :
+        CountP1 =+ len(Player1)
+        CountP2 =+ len(Player2)
         print(f"In {x} {P1Name} won {CountP1} times, {P2Name} won {CountP2} times !")
-        TotalP1 += len(Nadal)
-        TotalP2 += len(Djokovic)
+        TotalP1 += len(Player1)
+        TotalP2 += len(Player2)
 print(f"{P1Name} won {TotalP1} times, while {P2Name} won {TotalP2} times !")
 
 
